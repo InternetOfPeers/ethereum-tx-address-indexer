40c267a158f501070233f1bab6eeaaf885972eb7
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2021-05-17 19:09:51 +0200

    Updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,16 +8,16 @@ module.exports = {
     node_args: [],
     exec_mode: "fork",
     args: [
-      "300",
-      "310",
-      "4"
+      "0",
+      "4805355",
+      "500"
     ],
     env: {
       "JSONRPC_URL": "https://mainnet.infura.io/v3/7c5f3fc0248844869d448e51394ce26e",
       "NODE_ENV": "development"
     },
     env_production: {
-      "JSONRPC_URL": "https://mainnet.infura.io/v3/7c5f3fc0248844869d448e51394ce26e",
+      "JSONRPC_URL": "http://127.0.0.1:8545",
       "NODE_ENV": "production",
     }
   }]
